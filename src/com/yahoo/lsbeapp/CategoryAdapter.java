1de60e89638d363b0999e2346791c0eadaa79cd3
pinank gogri <pinank.gogri@gmail.com>
2013-11-04 17:14:16 -0800

    added map button

--- src/com/yahoo/lsbeapp/CategoryAdapter.java
+++ src/com/yahoo/lsbeapp/CategoryAdapter.java
@@ -3,16 +3,14 @@ package com.yahoo.lsbeapp;
 
 import java.util.List;
 
-import com.yahoo.lsbeapp.R;
-
-import com.yahoo.lsbeapp.model.Category;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.yahoo.lsbeapp.model.Category;
 
 public class CategoryAdapter extends ArrayAdapter<Category> {
 
